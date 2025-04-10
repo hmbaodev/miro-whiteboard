9cd6a3e14735bc35d54d7921769db702f43e4d61
Minh Bao <minhbao@Minhs-Laptop.local>
2025-04-10 11:46:30 +0700

    feat(toolbar): add toolbar icons ui

--- app/boards/[boardId]/_components/canvas-loading.tsx
+++ app/boards/[boardId]/_components/canvas-loading.tsx
@@ -1,18 +1,18 @@
-import { Loader } from "lucide-react"
+import { Loader } from "lucide-react";
 
-import { InfoSkeleton } from "./info"
-import {ParticipantsSkeleton} from "./participants"
+import { InfoSkeleton } from "./info";
+import { ParticipantsSkeleton } from "./participants";
 import { ToolbarSkeleton } from "./toolbar";
 
 const CanvasLoading = () => {
   return (
-    <main className="size-full relative bg-neutral-100 touch-none flex items-center justify-center">
-      <Loader className="size-6 text-muted-foreground animate-spin" />
+    <main className="relative flex size-full touch-none items-center justify-center bg-neutral-100">
+      <Loader className="text-muted-foreground size-6 animate-spin" />
       <InfoSkeleton />
       <ParticipantsSkeleton />
       <ToolbarSkeleton />
     </main>
-  )
-}
+  );
+};
 
-export default CanvasLoading
+export default CanvasLoading;
--- app/boards/[boardId]/_components/toolbar.tsx
+++ app/boards/[boardId]/_components/toolbar.tsx
@@ -1,15 +1,70 @@
+import {
+  Circle,
+  Pencil,
+  MousePointer2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+  Redo2,
+} from "lucide-react";
+
+import ToolButton from "./tool-button";
+
 const Toolbar = () => {
   return (
     <div className="absolute top-[50%] left-2 flex -translate-y-[50%] flex-col gap-y-4">
       <div className="flex flex-col items-center gap-y-1 rounded-md bg-white p-1.5 shadow-md">
-        <div>Pencil</div>
-        <div>Square</div>
-        <div>Circle</div>
-        <div>Ellipse</div>
+        <ToolButton
+          label="Select"
+          icon={MousePointer2}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Text"
+          icon={Type}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Sticky Note"
+          icon={StickyNote}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Rectangle"
+          icon={Square}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Ellipse"
+          icon={Circle}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Pen"
+          icon={Pencil}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
       <div className="flex flex-col items-center rounded-md bg-white p-1.5 shadow-md">
-        <div>Undo</div>
-        <div>Redo</div>
+        <ToolButton
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isActive={true}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isActive={true}
+        />
       </div>
     </div>
   );
