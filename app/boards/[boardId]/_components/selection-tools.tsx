6ecceced12f38e8c5378fb4a13a8c7538318ff2d
Minh Bao <minhbao@Minhs-Laptop.local>
2025-04-21 19:42:50 +0700

    feat(delete layer): add deleting layers feature

--- app/boards/[boardId]/_components/selection-tools.tsx
+++ app/boards/[boardId]/_components/selection-tools.tsx
@@ -2,10 +2,14 @@
 
 import { memo } from "react";
 import { useMutation, useSelf } from "@liveblocks/react";
+import { Trash2 } from "lucide-react";
 
 import { Camera, Color } from "@/types/canvas";
+import Hint from "@/components/hint";
+import { Button } from "@/components/ui/button";
 import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import ColorPicker from "./color-picker";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
 
 interface SelectionToolsProps {
   camera: Camera;
@@ -16,16 +20,21 @@ const SelectionTools = memo(
   ({ camera, setLastUsedColor }: SelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection);
 
+    const deleteLayers = useDeleteLayers();
+
     const selectionBounds = useSelectionBounds();
 
-    const setFill = useMutation(({ storage }, fill: Color) => {
-      const liveLayers = storage.get("layers");
-      setLastUsedColor(fill);
+    const setFill = useMutation(
+      ({ storage }, fill: Color) => {
+        const liveLayers = storage.get("layers");
+        setLastUsedColor(fill);
 
-      selection?.forEach((id) => {
-        liveLayers.get(id)?.set("fill", fill);
-      });
-    }, [selection, setLastUsedColor]);
+        selection?.forEach((id) => {
+          liveLayers.get(id)?.set("fill", fill);
+        });
+      },
+      [selection, setLastUsedColor],
+    );
 
     if (!selectionBounds) return null;
 
@@ -40,6 +49,13 @@ const SelectionTools = memo(
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className="ml-2 flex items-center border-l border-neutral-200 pl-2">
+          <Hint label="Delete" asChild>
+            <Button variant="board" size="icon" onClick={deleteLayers}>
+              <Trash2 />
+            </Button>
+          </Hint>
+        </div>
       </div>
     );
   },
